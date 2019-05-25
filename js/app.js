bcf37147baab9a13a16b374efe4aaaf12c473fff
cherbear263 <cheryl@realsocial.com.au>
2019-05-26 09:00:18 +1000

    fix html for can image

--- js/app.js
+++ js/app.js
@@ -37,8 +37,25 @@ var CocaCola = /** @class */ (function () {
     }
     return CocaCola;
 }());
+/// <reference path="product.ts" />
+var productFactory = /** @class */ (function () {
+    function productFactory() {
+    }
+    productFactory.GetProduct = function () {
+        return new CocaCola();
+    };
+    return productFactory;
+}());
 /// <reference path="./coin.ts" />
 /// <reference path="./product.ts" />
+/// <reference path="./productFactory.ts" />
+/// <reference path="../node_modules/@types/knockout/index.d.ts" />
+var VendingMachineSize;
+(function (VendingMachineSize) {
+    VendingMachineSize[VendingMachineSize["small"] = 6] = "small";
+    VendingMachineSize[VendingMachineSize["medium"] = 9] = "medium";
+    VendingMachineSize[VendingMachineSize["large"] = 12] = "large";
+})(VendingMachineSize || (VendingMachineSize = {}));
 var Cell = /** @class */ (function () {
     function Cell(product) {
         this.product = product;
@@ -51,18 +68,51 @@ var VendingMachine = /** @class */ (function () {
     function VendingMachine() {
         var _this = this;
         this.paid = ko.observable(0);
+        this.selectedCell = ko.observable(new Cell(new CocaCola()));
+        this.cells = ko.observableArray([]);
         this.acceptedCoins = [new Dollar()];
+        this.canPay = ko.pureComputed(function () { return _this.paid() - _this.selectedCell().product.price >= 0; });
+        this.select = function (cell) {
+            cell.sold(false);
+            _this.selectedCell(cell);
+        };
         this.acceptCoin = function (coin) {
             var oldTotal = _this.paid();
             _this.paid(oldTotal + coin.Value);
+        };
+        this.pay = function () {
+            if (_this.selectedCell().stock() < 1) {
+                alert("I'm sorry, we're out of them!");
+                return;
+            }
+            var currentPaid = _this.paid();
+            _this.paid(Math.round(((currentPaid -
+                _this.selectedCell().product.price) * 100)) / 100);
+            var currentStock = _this.selectedCell().stock();
+            _this.selectedCell().stock(currentStock - 1);
+            _this.selectedCell().sold(true);
         };
         /** "this" refers to the class, because this is an arrow function.
          * when a coin is accepted, the value of the coin is added to the "paid" count
          */
     }
+    Object.defineProperty(VendingMachine.prototype, "size", {
+        set: function (givenSize) {
+            this.cells([]);
+            for (var index = 0; index < givenSize; index++) {
+                var product = productFactory.GetProduct();
+                this.cells.push(new Cell(product));
+            }
+            ;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return VendingMachine;
 }());
 /// <reference path="vendingMachine.ts" />
+/// <reference path="../node_modules/@types/knockout/index.d.ts" />
 var machine = new VendingMachine();
+machine.size = VendingMachineSize.medium;
 ko.applyBindings(machine);
 //# sourceMappingURL=app.js.map
--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,6 +1,14 @@
 /// <reference path="./coin.ts" />
 /// <reference path="./product.ts" />
+/// <reference path="./productFactory.ts" />
+/// <reference path="../node_modules/@types/knockout/index.d.ts" />
 
+
+enum VendingMachineSize {
+    small = 6,
+    medium = 9,
+    large = 12
+}
 class Cell {
     constructor (public product: CocaCola) {
 
@@ -9,14 +17,44 @@ class Cell {
     sold = ko.observable(false)
 }
 class VendingMachine {
-    private paid = ko.observable(0);
+    paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()))
+    cells = ko.observableArray([])
     acceptedCoins: Dollar[] = [new Dollar()]
+    canPay = ko.pureComputed(()=> this.paid() - this.selectedCell().product.price >=0)
+
+    set size(givenSize: VendingMachineSize) {
+        this.cells([]);
+        for (let index = 0; index<givenSize; index++) {
+            let product = productFactory.GetProduct();
+            this.cells.push(new Cell(product));
+        };
+    }
+    select = (cell: Cell): void => {
+       cell.sold(false)
+       this.selectedCell(cell)
+    }
+
     acceptCoin = (coin: Dollar): void  => {
         let oldTotal = this.paid()
         this.paid(oldTotal + coin.Value)
         
     }
+    pay = (): void => {
+        if ( this.selectedCell().stock() < 1) {
+            alert("I'm sorry, we're out of them!")
+            return
+        }
+        let currentPaid = this.paid()
+        this.paid(Math.round(((currentPaid - 
+            this.selectedCell().product.price)*100))/100)
+            let currentStock = this.selectedCell().stock()
+            this.selectedCell().stock(currentStock - 1)
+            this.selectedCell().sold(true)
+    }
     /** "this" refers to the class, because this is an arrow function.
      * when a coin is accepted, the value of the coin is added to the "paid" count
      */
+
+
 }
